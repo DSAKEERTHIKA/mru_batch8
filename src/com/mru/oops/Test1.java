c00217d3c53913c4331cbef881927b22fa54afc9
DSAKEERTHIKA <nathalakeerthikareddy8@gmail.com>
2025-01-10 14:32:23 +0530

    oops

---
+++ src/com/mru/oops/Test1.java
@@ -0,0 +1,33 @@
+package com.mru.oops;
+
+class Test1 implements Interface1{
+	@Override
+	public void addition() {
+		int num1 = 100;
+		int num2 = 50;
+		int res = num1 + num2;
+		System.out.println("Addition...."+res);		
+	}
+
+	@Override
+	public int subtraction(int num1, int num2) {
+		int res = num1 - num2;
+		return res;
+	}
+
+	@Override
+	public void multiplication(int num1, int num2) {
+		int res = num1 * num2;
+		System.out.println("Multiplication...."+res);
+		
+	}
+
+	@Override
+	public int division() {
+		int num1 = 100;
+		int num2 = 50;
+		int res = num1/num2;
+		return res;
+	}
+}
+
