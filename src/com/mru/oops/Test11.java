b72d0ec4170a53a9df4cfe995bfb75877c6f0d2a
DSAKEERTHIKA <nathalakeerthikareddy8@gmail.com>
2025-01-11 20:34:38 +0530

    oops

---
+++ src/com/mru/oops/Test11.java
@@ -0,0 +1,9 @@
+package com.mru.oops;
+
+public class Test11 {
+	String message = "keerthika reddy";
+	public void m1() {
+		System.out.println("keerthika1");
+	}
+
+}
---
+++ src/com/mru/oops/Test12.java
@@ -0,0 +1,9 @@
+package com.mru.oops;
+
+public class Test12 {
+	String msg = "Likhitha reddy";
+	public void m2() {
+		System.out.println("Likhitha1");
+	}
+
+}
